c3dd54d61692c49550c14005aca48395dfc5741b
Alex Szabo <alex.szabo@elastic.co>
2023-06-14 22:29:59 +0200

    Strip -glibc-217 variant from filenames to keep original naming

--- scripts/upload_artifacts.sh
+++ scripts/upload_artifacts.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-set -euox pipefail
+set -euo pipefail
 
 # TARGET_VERSION provided in env
 
 BUCKET_NAME="kibana-custom-node-artifacts"
 ARTIFACT_BASE_PATH="node-glibc-217/dist/v$TARGET_VERSION/"
+ARTIFACT_DIST_DIR="./workdir/dist"
+
+echo "--- Removing variant tags from file names"
+for name in $(ls $ARTIFACT_DIST_DIR | grep tar); do
+  NAME_STRIPPED=$(echo "$name" | sed s/-glibc-217//)
+  mv $ARTIFACT_DIST_DIR/$name $ARTIFACT_DIST_DIR/$NAME_STRIPPED
+done
 
 echo "--- Uploading build artifacts"
-gsutil cp -r "./workdir/dist/*" gs://$BUCKET_NAME/$ARTIFACT_BASE_PATH
+gsutil cp -r "$ARTIFACT_DIST_DIR/*" gs://$BUCKET_NAME/$ARTIFACT_BASE_PATH
