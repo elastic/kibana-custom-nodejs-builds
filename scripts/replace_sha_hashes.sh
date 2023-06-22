702ca66622d4765352b09ce07fe08c66943dc9f7
Alex Szabo <alex.szabo@elastic.co>
2023-06-22 13:20:05 +0200

    Add SHASUM fixing as a last step after both agents finished

---
+++ scripts/replace_sha_hashes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+source ./scripts/common.sh
+
+# TARGET_VERSION provided in env
+
+BUCKET_NAME="kibana-custom-node-artifacts"
+ARTIFACT_BASE_PATH="node-glibc-217/dist/v$TARGET_VERSION"
+SHASUMS_LOCATION="https://nodejs.org/dist/v$TARGET_VERSION/SHASUMS256.txt"
+
+ARTIFACT_DIST_DIR="./workdir/download"
+
+echo "--- Downloading node.js $TARGET_VERSION glibc-217 artifacts"
+mkdir -p $ARTIFACT_DIST_DIR
+
+gsutil cp -r gs://$BUCKET_NAME/$ARTIFACT_BASE_PATH/* "$ARTIFACT_DIST_DIR/"
+
+echo "--- Downloading SHASUMS256.txt"
+retry 5 10 curl -fsSLO $SHASUMS_LOCATION
+mv SHASUMS256.txt $ARTIFACT_DIST_DIR/SHASUMS256.txt
+
+echo "--- Replacing checksums with local file hashes"
+replace_shasums_in_folder $ARTIFACT_DIST_DIR
+
+echo "--- Uploading SHASUMS256.txt to $BUCKET_NAME"
+gsutil cp -r "$ARTIFACT_DIST_DIR/SHASUMS256.txt" gs://$BUCKET_NAME/$ARTIFACT_BASE_PATH/
--- scripts/upload_nodejs_artifacts.sh
+++ scripts/upload_nodejs_artifacts.sh
@@ -8,18 +8,6 @@ source ./scripts/common.sh
 BUCKET_NAME="kibana-custom-node-artifacts"
 ARTIFACT_BASE_PATH="node-glibc-217/dist/v$TARGET_VERSION/"
 ARTIFACT_DIST_DIR="./workdir/dist"
-SHASUMS_LOCATION="https://nodejs.org/dist/v$TARGET_VERSION/SHASUMS256.txt"
-
-# echo "--- Removing variant tags from file names"
-# for name in $(ls $ARTIFACT_DIST_DIR | grep tar); do
-#   NAME_STRIPPED=$(echo "$name" | sed s/-glibc-217//)
-#   mv $ARTIFACT_DIST_DIR/$name $ARTIFACT_DIST_DIR/$NAME_STRIPPED
-# done
-
-echo "--- Replacing SHASUMS256.txt entries"
-retry 5 10 curl -fsSLO $SHASUMS_LOCATION
-mv SHASUMS256.txt $ARTIFACT_DIST_DIR/SHASUMS256.txt
-replace_shasums_in_folder $ARTIFACT_DIST_DIR
 
 echo "--- Uploading build artifacts"
 gsutil cp -r "$ARTIFACT_DIST_DIR/*" gs://$BUCKET_NAME/$ARTIFACT_BASE_PATH
