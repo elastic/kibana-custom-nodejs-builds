fecc714ab61d36ca17ed36f271792100c841698a
Alex Szabo <alex.szabo@elastic.co>
2023-06-22 16:18:01 +0200

    Remove unnecessary files, add README/Context

--- scripts/docker_login.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-registry_user=$(vault read -field=username secret/ci/elastic-ci-agent-images/docker-registry)
-registry_password=$(vault read -field=password secret/ci/elastic-ci-agent-images/docker-registry)
-
-echo -n "Logging in to docker.elastic.co as ${registry_user}... "
-docker login --username="${registry_user}" --password="${registry_password}" docker.elastic.co
