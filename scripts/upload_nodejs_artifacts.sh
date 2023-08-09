1ae8dd4c8be0ab1f1cb8d4a76a9c150dc5330907
Alex Szabo <alex.szabo@elastic.co>
2023-08-09 19:52:16 +0200

    Log checksums in builds before uploading

--- scripts/upload_nodejs_artifacts.sh
+++ scripts/upload_nodejs_artifacts.sh
@@ -8,5 +8,7 @@ source ./scripts/common.sh
 ARTIFACT_BASE_PATH="node-glibc-217/dist/v$TARGET_VERSION/"
 ARTIFACT_DIST_DIR="./workdir/dist"
 
+list_shasums_in_folder $ARTIFACT_DIST_DIR
+
 echo "--- Uploading build artifacts"
 gsutil cp -r "$ARTIFACT_DIST_DIR/*" gs://$BUCKET_NAME/$ARTIFACT_BASE_PATH
--- scripts/upload_re2_artifacts.sh
+++ scripts/upload_re2_artifacts.sh
@@ -7,5 +7,7 @@ RE2_FULL_VERSION=$RE2_VERSION # $1
 ARTIFACT_BASE_PATH="re2-glibc-217/$RE2_FULL_VERSION/"
 ARTIFACT_DIST_DIR="./workdir_re2/dist"
 
+list_shasums_in_folder $ARTIFACT_DIST_DIR
+
 echo "--- Uploading build artifacts"
 gsutil cp -r "$ARTIFACT_DIST_DIR/*" gs://$BUCKET_NAME/$ARTIFACT_BASE_PATH
