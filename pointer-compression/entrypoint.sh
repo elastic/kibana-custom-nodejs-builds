81d9e8fc9ce88794d898f682a4e07c4eb26384cf
Jonathan Budzenski <jon@elastic.co>
2025-07-21 18:10:58 -0500

    try 10

--- pointer-compression/entrypoint.sh
+++ pointer-compression/entrypoint.sh
@@ -20,8 +20,8 @@ cd "/home/node/workdir/src/node-${full_version}"
 
 # Compile from source
 export CCACHE_DIR="/home/node/workdir/.ccache-${architecture}"
-export CC="ccache gcc"
-export CXX="ccache g++"
+export CC="ccache gcc-10"
+export CXX="ccache g++-10"
 
 make -j"$(getconf _NPROCESSORS_ONLN)" binary V= \
   DESTCPU="$architecture" \
